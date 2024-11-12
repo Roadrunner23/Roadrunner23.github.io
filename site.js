a6efacbb19c45517521c2efa9f23ad930e58935a
Roadrunner23 <andread115@icloud.com>
2024-11-12 12:54:16 -0500

    Finished basic layout of index and completed contact and experience sections

---
+++ site.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Sets up the bootstrap accordions under the "Projects" heading of index.
+    accordionSetup();
+});
+
+function accordionSetup() {
+    const header1 = document.querySelector('.header1');
+}
