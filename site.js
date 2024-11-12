66a39a3f0f3bc2247ac33af101d91e0199fd5f72
Roadrunner23 <andread115@icloud.com>
2024-11-12 16:57:26 -0500

    Finished rest of index. All thats left is implementing specific pages for each Project

--- site.js
+++ site.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Sets up the bootstrap accordions under the "Projects" heading of index.
-    accordionSetup();
+    ProjectAnimationSetup();
 });
 
-function accordionSetup() {
-    const header1 = document.querySelector('.header1');
+function ProjectAnimationSetup() {
+    document.querySelectorAll('button').forEach((button) => {
+        button.addEventListener('click', () => {
+            if (button.getAttribute('aria-expanded') == 'true') {
+                button.style.animationDuration = '0.01s';
+                button.classList.remove('radiusout');
+                button.classList.add('radiusin');
+            } else {
+                button.style.animationDuration = '1s';
+                button.classList.remove('radiusin');
+                button.classList.add('radiusout');
+            }
+        })
+    })
 }
