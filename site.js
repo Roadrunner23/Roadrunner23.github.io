bae68bf84eaef3148262e0ebb7a16a90536b1a2a
Roadrunner23 <andread115@icloud.com>
2024-11-13 17:02:01 -0500

    Implemented JS functionality to go to other pages and fixed bugs

--- site.js
+++ site.js
@@ -1,7 +1,56 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    window.mainpage = document.querySelector('#mainpage');
+
+    window.projectpage = document.querySelector('#projectpage');
+    projectpage.style.display = 'none';
+
+    // Sets up the return anchor
+    const goBack = document.querySelector('#return');
+    goBack.addEventListener('click', () => {
+
+        document.body.scrollTop = 0;
+
+        const desc = document.querySelector('.description');
+        desc.firstElementChild.style.display = 'block';
+
+        goBack.style.display = 'none';
+
+        mainpage.style.display = 'inline';
+        projectpage.style.display = 'none';
+    })
+
     // Sets up the bootstrap accordions under the "Projects" heading of index.
     ProjectAnimationSetup();
+
+    document.querySelectorAll('.projectlink').forEach((a) => {
+        a.addEventListener('click', () => {
+            
+            document.body.scrollTop = 0;
+
+            const card = a.parentElement;
+            const button = a.parentElement.parentElement.parentElement.firstElementChild;
+
+            PageSetup(button, card);
+        })
+    })
 });
+
+function PageSetup(button, card) {
+    mainpage.style.display = 'none';
+    projectpage.style.display = 'inline';
+
+    const desc = document.querySelector('.description');
+    desc.firstElementChild.style.display = 'none';
+
+    const goBack = document.querySelector('#return');
+    goBack.style.display = 'block';
+
+    var name = button.firstElementChild.innerHTML;
+    if (name.slice(0, 3) == '<u>') {
+        name = name.slice(3, -4);
+    }
+}
 
 function ProjectAnimationSetup() {
     document.querySelectorAll('button').forEach((button) => {
